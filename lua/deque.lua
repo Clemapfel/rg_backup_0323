f49593bcb7e5986c6bd73d0edb4ad55f9d1141b2
clem <mail@clemens-cords.com>
2023-01-29 16:03:42 +0100

    stashing lua typesystem

--- lua/deque.lua
+++ lua/deque.lua
@@ -3,29 +3,39 @@ function Vector()
     out = {}
     
     out.push_back = function(this, value)
-        current = this.__priv.n
+        current = this.__priv.i
         this.__priv.data[current] = value
-        this.__priv.n = current + 1
+        this.__priv.i = current + 1
+        this.__priv.size = this.__priv.size + 1
+    end
+
+    out.resize = function(this, n)
+        for i = this.__priv.size, n, 1 do
+            this.__priv.data[i] = 0
+        end
+        this.__priv.size = n
     end
 
     out.size = function(this)
-        return this.__priv.n
+        return this.__priv.size
     end
 
     out.clear = function(this)
-        this.__priv.n = 0
+        this.__priv.i = 1
+        this.__priv.size = 1
         this.__priv.data = {}
     end
     
     meta = {}
-    meta.n = 0
+    meta.i = 1
+    meta.size = 0
     meta.data = {}
 
     meta.__index = function(this, key)
 
         if (type(key) ~= "number") then return nil end
-        if (key >= this.__priv.n) then
-            error("Key " .. tostring(key) .. " is out of bounds for a vector of size " .. this.__priv.n)
+        if (key > this.__priv.size or key < 1) then
+            error("Key " .. tostring(key) .. " is out of bounds for a vector of size " .. this.__priv.size)
             return nil
         end
 
@@ -34,29 +44,37 @@ function Vector()
     
     meta.__newindex = function(this, key, value)
 
-        if (type(key) ~= "number") then return nil end
-        error("Key " .. tostring(key) .. " is out of bounds for a vector of size " .. this.__priv.n)
-        return nil
+        if (type(key) ~= "number" or (type(key) == "number" and math.floor(key) ~= key)) then
+            return nil
+        end
+        if (key > this.__priv.size or key < 1) then
+            error("Key " .. tostring(key) .. " is out of bounds for a vector of size " .. this.__priv.size)
+            return nil
+        end
+
+        this.__priv.data[key] = value
     end
 
     meta.__len = function(this)
-        return this.__priv.n
+        return this.__priv.size
     end
 
     meta.__pairs = function(this)
 
         local function stateless_iterator(this, i)
 
-            if i == this.__priv.n then
+            if i == this.__priv.size then
                 return nil, nil
             else
-                return i+1, this.__priv.data[i]
+                return i+1, this.__priv.data[i+1]
             end
         end
 
         return stateless_iterator, this, 0
     end
-   
+
+    meta.__ipairs = ipairs(meta.data)
+
     out.__priv = meta
     setmetatable(out, out.__priv)
     
@@ -67,6 +85,7 @@ vector = Vector()
 vector:push_back(12)
 vector:push_back(13)
 vector:push_back(14)
+vector:resize(10)
 
 for i, v in pairs(vector) do
     print(tostring(i) .. " " .. tostring(v))
--- lua/meta.lua
+++ lua/meta.lua
@@ -1,42 +1,85 @@
+meta = {}
+meta.detail = {}
+meta.detail.n_types = 0
+meta.detail.type_instances = {}
 
---- @brief print warning to cerr
---- @param message string
-function warning(message)
-    io.stderr.write("[WARNING]" .. message)
+function meta.detail.add_type(type_name)
+
+    local id = meta.detail.n_types
+    meta.detail.type_ids[id] = type_name
+    meta.detail.n_types = id + 1
+    return id
 end
 
----
-function new_prototype(name, table)
-
-    if _G["mousetrap"] == nil then
-        _G["mousetrap"] = {}
-    end
-
-    if _G["mousetrap"]["types"] == nil then
-        _G["mousetrap"]["types"] = []
-    end
-
-    _G["mousetrap"]["types"]
-
+function meta.detail.get_type(id)
+    return meta.detail.type_ids[id]
 end
 
+function meta.typeof(object)
+    return meta.detail.type_ids[object.__meta.type]
+end
 
-BattleEntity_t = {
-    health = 0,
-    ap = 0,
+function meta.isa(object, type)
+    return meta.detail.type_ids[object.__meta.type] == type
+end
 
-    attack = 0,
-    defense = 0,
-    speed = 0
-}
+function meta.add_property(type, property_name, property_value)
 
-function new_battle_entity()
+    type.__meta.properties[property_name] = property_value
+    rawset(type, "set_" .. property_name, function(this, x) this.__meta.properties[property_name] = x end)
+    rawset(type, "get_" .. property_name, function(this) return this.__meta.properties[property_name] end)
+end
+
+function meta.add_const_property(type, property_name, property_value)
+
+    type.__meta.const_properties[property_name] = property_value
+    rawset(type, "get_" .. property_name, function(this) return this.__meta.const_properties[property_name] end)
+end
+
+function meta.add_method(type)
+
+function meta.new_type(type_name)
 
     out = {}
-    out.__meta = {
-        __index = BatteEntity_t,
-        __newindex = function(instance, key, value)
-            warning("Attempting to write ")
+    out.__meta = {}
+
+    out.__meta.const_properties = {}
+    out.__meta.properties = {}
+    out.__meta.type = meta.detail.add_type(type_name)
+
+    out.__meta.__index = function(this, key)
+
+        x = this.__meta.properties[key]
+        if (x == nil) then
+            return this.__meta.const_properties
+        else
+            return x
         end
-    }
+    end
+
+    out.__meta.__newindex = function(this, key, value)
+
+        if (this.__meta.properties[key] ~= nil) then
+            this.__meta.properties[key] = value
+        elseif this.__meta.const_properties[key] ~= nil then
+            error("In __newindex for object of type " .. meta.typeof(this) .. ": Trying to assign property \"" .. key .. "\" but it is declared const")
+        else
+            error("In __newindex for object of type " .. meta.typeof(this) .. ": No property with name \"" .. key .. "\". Use meta.add_property instead")
+        end
+    end
+
+    setmetatable(out, out.__meta)
+    meta.detail.type_instances[]
 end
+
+-- TEST
+instance = meta.new_type("Test_t")
+meta.add_property(instance, "mutable", 12)
+meta.add_const_property(instance, "immutable", 13)
+
+print(instance:get_mutable())
+instance:set_mutable(19)
+print(instance:get_mutable())
+
+print(instance:get_immutable())
+instance["immutable"] = 12
